e3b96a1a9617d479b5111b2e38f0c90879cdc7c0
Ryang Sohn <loop.infinitely@gmail.com>
2022-01-24 15:03:13 +0900

    feat: add database connection to resource 'users'

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot({ type: 'postgres', url: process.env.DATABASE_URL }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
