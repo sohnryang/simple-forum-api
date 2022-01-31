7b17b9689a664fa776902b7682c6d9892325a99c
Ryang Sohn <loop.infinitely@gmail.com>
2022-01-31 17:44:18 +0900

    feat: implement `User` entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,22 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  username: string;
+
+  @Column({ type: 'date' })
+  birthday: string;
+
+  @Column()
+  passwordHash: string;
+
+  @Column()
+  salt: string;
+}
