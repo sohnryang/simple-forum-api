a4bf8081962f9558d64b6ebf6ae94114e351d355
Ryang Sohn <loop.infinitely@gmail.com>
2022-02-05 10:10:10 +0900

    feat: add `Forumpost` entity

--- src/forumposts/entities/forumpost.entity.ts
+++ src/forumposts/entities/forumpost.entity.ts
@@ -1,1 +1,19 @@
-export class Forumpost {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Forumpost {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  authorId: number;
+
+  @Column()
+  title: string;
+
+  @Column({ type: 'text' })
+  content: string;
+
+  @Column()
+  hashtags: string;
+}
